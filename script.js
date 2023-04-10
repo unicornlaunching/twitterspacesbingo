596ff4686d705d463e29fc5fa6f735315414a4b4
unicornlaunching <45397093+unicornlaunching@users.noreply.github.com>
2023-04-10 00:31:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,47 @@
-// Get the table cells and input field
-const cells = document.querySelectorAll('td');
-const input = document.querySelector('input');
+// Get the table cells and the generate button
+var cells = document.querySelectorAll('td');
+var generateButton = document.getElementById('generate-button');
 
-// List of default values
-const defaultValues = ['Danologue', 'IIWII', 'Ally', 'Talking Over ERich', 'Peter Waxes Philosphical', 'Well Im Broken', 'Matios Joins', 'Regulating AI', 'Overton Window', 'Python', 'Right!?', 'Evil Manish', 'Alli Bio Fact', 'Unneccessary Bye', 'Zach Disagrees', 'ERich Introduces Friend', 'Crow Crows', 'Dan Club', 'Alaskoshi Alaskoshis', 'Ello Aw Waw Wii', 'Room Rugged'];
+// Add an event listener to the generate button
+generateButton.addEventListener('click', function() {
+  var inputValues = document.getElementById('input-values').value;
+  var values = inputValues.split(',').map(function(value) {
+    return value.trim();
+  });
 
-// Shuffle array and return a random subset of n values
-function getRandomValues(values, n) {
-  const shuffled = values.sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, n);
-}
+  // Shuffle the array and select a random subset of 9 values
+  var shuffledValues = shuffle(values).slice(0, 9);
 
-// Populate cells with shuffled values
-function populateCells(values) {
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].textContent = values[i];
+  // Fill the cells with the selected values
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].textContent = shuffledValues[i];
+    cells[i].classList.remove('active');
+  }
+});
 
-//    cells[i].addEventListener('click', function () {
-//      this.classList.toggle('active');
-//    });
-//  }
- 
-      cells[i].addEventListener('click', function() {
+// Add an event listener to each cell
+cells.forEach(function(cell) {
+  cell.addEventListener('click', function() {
     if (this.classList.contains('active')) {
       this.classList.remove('active');
     } else {
       this.classList.add('active');
     }
   });
-}
-
-// Listen for user input and generate table on button click
-document.querySelector('button').addEventListener('click', () => {
-  const userInput = input.value.trim().split(',').map(value => value.trim());
-
-  // Use default values if user input is empty or invalid
-  const values = userInput.length > 0 && userInput[0] !== '' ? getRandomValues(userInput, 9) : getRandomValues(defaultValues, 9);
-
-  populateCells(values);
 });
 
-// Listen for cell click events
-cells.forEach(cell => {
-  cell.addEventListener('click', () => {
-    cell.classList.toggle('pressed');
-  });
-});
-
-// Shuffle array in place
+// Shuffle the array
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+  var currentIndex = array.length, randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]
+    ];
   }
+
+  return array;
 }
-
-// Shuffle default values array on page load
-shuffle(defaultValues);
-
-
-
-
-
-
-
-
-
-
-
-
